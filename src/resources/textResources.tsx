8abed689b37fbc37649a03c3f87103ebbfdd469e
orlowski-dev <orlowski.dev@gmail.com>
2023-08-20 20:07:20 +0200

    Last update

--- src/resources/textResources.tsx
+++
@@ -1,5 +0,0 @@
-export const textResources = ({
-    heroH1: "Hi, I'm Patryk Orłowski",
-    heroH2: "Im a 23 year old guy who tries his hand at web-dev",
-    heroP: "I'm currently learning JavaScript and ReactJS by doing challenges from Frontned Mentor webiste and from udemy course. I also have some episodes with Python Django Framework and PHP, but I decided to focus on front-end development.. for now."
-})
